fa056aabe5f3cfa04f17c7a85d283553516e5ea0
Jonah Brüchert <jbb.prv@gmx.de>
2019-01-20 14:46:38 +0100

    Final index fixes

--- createindex.sh
+++ createindex.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tree -P '*.xz|*.gz|*.tar|*.bz2' -H https://debian-pm-tools.github.io/orig-tar-xzs ${COMMON_ARGS} . > index.html
-tree -J '*.xz|*.gz|*.tar|*.bz2' ${COMMON_ARGS} . > index.json
+tree -P '*.xz|*.gz|*.tar|*.bz2' -H https://debian-pm-tools.github.io/orig-tar-xzs . > index.html
+tree -J -P '*.xz|*.gz|*.tar|*.bz2' . > index.json
