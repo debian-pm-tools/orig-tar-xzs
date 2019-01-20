09a6baafceba1094b514fa46b939e7a3c123b77e
Jonah Brüchert <jbb.prv@gmx.de>
2019-01-20 14:49:28 +0100

    Prettyprint json

--- createindex.sh
+++ createindex.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 tree -P '*.xz|*.gz|*.tar|*.bz2' -H https://debian-pm-tools.github.io/orig-tar-xzs . > index.html
-tree -J -P '*.xz|*.gz|*.tar|*.bz2' . > index.json
+tree -J -P '*.xz|*.gz|*.tar|*.bz2' . | python3 -m json.tool > index.json
