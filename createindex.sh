29a5ef2016971465d96a3a47e1a8c5b426235bcf
Jonah Brüchert <jbb.prv@gmx.de>
2019-01-20 14:42:52 +0100

    Update index generation script

--- createindex.sh
+++ createindex.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-tree -H https://debian-pm-tools.github.io/orig-tar-xzs > index.html
+tree -P '*.xz|*.gz|*.tar|*.bz2' -H https://debian-pm-tools.github.io/orig-tar-xzs ${COMMON_ARGS} . > index.html
+tree -J '*.xz|*.gz|*.tar|*.bz2' ${COMMON_ARGS} . > index.json
